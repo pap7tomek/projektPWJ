9107ccd06e8c26b925ced7295ee62211f0268d0c
pap7tomek <pap7tomek@gmail.com>
2018-06-04 00:35:06 +0200

    połączenie z bazą oraz tabela logs

---
+++ src/Logs.java
@@ -0,0 +1,44 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "logs")
+public class Logs {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+
+    private String secoundName;
+
+    private String phoneNumber;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecoundName() {
+		return secoundName;
+	}
+
+	public void setSecoundName(String secoundName) {
+		this.secoundName = secoundName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+    
+}
